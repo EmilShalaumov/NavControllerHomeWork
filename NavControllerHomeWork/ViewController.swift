c2be27ee4b3381c609972e9eff1bf3aa7bf412e9
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-10-19 12:38:52 +0300

    Initial Commit

---
+++ NavControllerHomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavControllerHomeWork
+//
+//  Created by Эмиль Шалаумов on 19.10.2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
